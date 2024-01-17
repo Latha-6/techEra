d1d04499a6b9968d62f16755333d7eaf9a59b81e
sneha <yysnehalatha@gmail.com>
2024-01-17 02:56:59 +0000

    solutions

---
+++ src/components/CourseItemDetails/style.js
@@ -0,0 +1,71 @@
+import styled from 'styled-components'
+
+export const Nel = styled.nav`
+   backgroung-color: #f1f5f9;
+   padding: 20px;
+   min-height: 10vh;
+   width:  100%;
+`
+export const Logo = styled.img`
+   height: 70px;
+   width: 100px;
+`
+
+export const View = styled.div`
+   display: flex;
+   flex-direction: row;
+   align-items: flex-start;
+   backgroung-color: #ffffff;
+   width: 80%;
+`
+export const Vi = styled.img`
+   height: 250px;
+   width: 250px;
+   margin-right: 10px;
+`
+
+export const Vd = styled.p`
+   color: #64748b;
+   font-family: 'Roboto';
+   font-size: 22px;
+`
+export const FailCom = styled.div`
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: center;
+   min-height: 100vh;
+`
+export const FailTm = styled.img`
+   height: 250px;
+   width: 250px;
+   margin-right: 16px;
+`
+
+export const Fh = styled.h1`
+   color: #1e293b;
+   font-size: 30px;
+   font-weight: bold;
+   font-family: 'Roboto';
+`
+
+export const Fp = styled.p`
+   color: #475569;
+   font-family: 'Roboto';
+   font-size: 22px;
+`
+export const Fb = styled.button`
+   background-color: #4655a1;
+   color: #ffffff;
+   font-family: 'Roboto';
+   border: none;
+   padding: 7px;
+   border-radius: 3px;
+`
+export const Vh = styled.h1`
+   color: #1e293b;
+   font-size: 30px;
+   font-family: 'Roboto';
+   margin-bottom: 7px;
+`
+export default Nel
---
+++ src/components/NotFound/style.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+
+export const NoCon = styled.div`
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: center;
+   min-height: 100vh;
+`
+
+export const NoIm = styled.img`
+    height: 250px;
+    width: 250px;
+`
+export const Nel = styled.nav`
+    backgroung-color: #f11f5f9;
+    padding: 20px;
+    min-height: 10vh;
+    width: 100%;
+`
+export const Logo = styled.img`
+   height: 70px;
+   width: 100px;
+`
+export const Noh = styled.h1`
+   color:#1e293b;
+   font-size: 30px;
+   font-family: 'Roboto';
+`
+export default Nop = styled.p`
+    color: #475569;
+    font-size: 16px;
+    font0family: 'Roboto';
+`
+export default NoCon
